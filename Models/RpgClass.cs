6187bee524d0bccea4ac872b84cabdc5308833d2
KHajili98 <khajili98@gmail.com>
2021-05-04 13:49:14 +0400

    adding new model

---
+++ Models/RpgClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNet_RPG.Models
+{
+    public enum RpgClass
+    {
+        Knight=1,
+        Mage=2,
+        Cleric=3
+    }
+}
