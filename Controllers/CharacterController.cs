545af7508205d7dae5f3526c446e885cb0b56be3
KHajili98 <khajili98@gmail.com>
2021-05-04 13:54:22 +0400

    characterController created

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,22 @@
+﻿using DotNet_RPG.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNet_RPG.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CharacterController : ControllerBase
+    {
+        private static Character knigh = new Character();
+
+        public IActionResult Get()
+        {
+            return Ok(knigh);
+        }
+    }
+}
