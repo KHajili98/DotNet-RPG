0204a0bfb37902a0a4b0b90e2ffabdcf3e3af687
KHajili98 <khajili98@gmail.com>
2021-05-12 14:53:36 +0400

    user authrepository created

---
+++ Data/IAuthRepository.cs
@@ -0,0 +1,16 @@
+﻿using DotNet_RPG.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNet_RPG.Data
+{
+    interface IAuthRepository
+    {
+        Task<ServiceResponse<int>> Register(User user, string password);
+        Task<ServiceResponse<string>> Login(string username, string password);
+        Task<bool> UserExists(string username);
+
+    }
+}
