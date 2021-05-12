06d3ebce9129e2eb44a905a6c5e3e83f66134c79
KHajili98 <khajili98@gmail.com>
2021-05-12 14:34:39 +0400

    users table added db

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -12,5 +12,6 @@ namespace DotNet_RPG.Data
         public DataContext(DbContextOptions<DataContext> options) :base(options)  { }
 
         public DbSet<Character> Characters { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
---
+++ Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNet_RPG.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public byte[] PaasswordHash { get; set; }
+        public byte[] PaasswordSalt { get; set; }
+    }
+}
